db1b5c0a18b1c47ff8505bdf965dadfd33e14936
Domenico Manna <mannadomenico2849@gmail.com>
2019-09-13 23:39:58 -0400

    Update theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,7 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         // background_color: `#663399`,
-        // theme_color: `#663399`,
+        theme_color: `#f6f9fc`,
         display: `minimal-ui`,
         icon: `src/assets/images/favicon.png`, // This path is relative to the root of the site.
       },
