1e65e2d23ca791b873aadd8e0b80e335113cf2b5
Domenico Manna <mannadomenico2849@gmail.com>
2020-08-20 18:47:25 -0400

    Adjust project wording

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -2,11 +2,11 @@ export default [
   {
     imageFileName: "bodyFitnessTracker.png",
     title: "Body Fitness Tracker",
-    description: 'A full stack webapp that allows users to track their body measurements and body fat percentage.',
+    description: 'A website powered by a REST API that allows users to track their body measurements and body fat percentage.',
     technologiesUsed: [
       ".Net Core",
       "Entity Framework Core",
-      "MySql",
+      "MySql Server",
       "Linux VPS",
       "React",
       "Typescript",
@@ -18,7 +18,7 @@ export default [
   {
     imageFileName: "beerHub.png",
     title: "BeerHub",
-    description: `A React app that utilizes the <a href = "https://punkapi.com/" target = "_blank" rel="noopener noreferrer"> Punk API </a> to retrieve information about BrewDog's massive beer catalog. This app enables users to search for any BrewDog beer, and save their favorite ones. Favorites are persisted through local storage.`,
+    description: `A React app that utilizes the <a href = "https://punkapi.com/" target = "_blank" rel="noopener noreferrer"> Punk API </a> to retrieve information about BrewDog's massive beer catalog. This app enables users to search for any BrewDog beer, and save their favorite ones.`,
     technologiesUsed: ["React", "CSS"],
     websiteLink: "https://domenicomanna.github.io/beerHub/",
     repositoryLink: "https://github.com/domenicomanna/beerHub",
@@ -26,7 +26,7 @@ export default [
   {
     imageFileName: "titanFitness.png",
     title: "Titan Fitness",
-    description: `A responsive, multipage website for a fictional gym. This project was created to practice fundamental CSS layouts such as flexbox and grid.`,
+    description: `A responsive website for a fictional gym. This project was created to practice fundamental CSS layouts such as flexbox and grid.`,
     technologiesUsed: ["Javascript", "CSS", "HTML"],
     websiteLink: "https://domenicomanna.github.io/titanFitness/",
     repositoryLink: "https://github.com/domenicomanna/titanFitness",
