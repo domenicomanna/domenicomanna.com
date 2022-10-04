05b4675ee9d9760f8020e2f831db23743bf85a19
dom <mannadomenico2849@gmail.com>
2022-10-04 18:54:39 -0400

    Compress images

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,6 +1,6 @@
 export default [
   {
-    imageFileName: "bodyFitnessTracker.png",
+    imageFileName: "bodyFitnessTracker.jpg",
     title: "Body Fitness Tracker",
     description:
       "A website powered by a REST API that allows users to track their body measurements and body fat percentage.",
@@ -17,7 +17,7 @@ export default [
     repositoryLink: "https://github.com/domenicomanna/bodyFitTracker",
   },
   {
-    imageFileName: "beerHub.png",
+    imageFileName: "beerHub.jpg",
     title: "BeerHub",
     description: `A React app that utilizes the <a href = "https://punkapi.com/" target = "_blank" rel="noopener noreferrer"> Punk API </a> to retrieve information about BrewDog's massive beer catalog. This app enables users to search for any BrewDog beer, and save their favorite ones.`,
     technologiesUsed: ["React", "CSS"],
@@ -25,7 +25,7 @@ export default [
     repositoryLink: "https://github.com/domenicomanna/beerHub",
   },
   {
-    imageFileName: "titanFitness.png",
+    imageFileName: "titanFitness.jpg",
     title: "Titan Fitness",
     description: `A responsive website for a fictional gym. This project was created to practice fundamental CSS layouts such as flexbox and grid.`,
     technologiesUsed: ["Javascript", "CSS", "HTML"],
