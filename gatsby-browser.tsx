845b1d42cc43d6c2f7bf63fd4befa8693c51a9f1
dom <mannadomenico2849@gmail.com>
2024-02-24 17:30:44 -0500

    Fix minified React error #418

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -3,6 +3,7 @@ import 'prismjs/themes/prism-tomorrow.css';
 import './src/global.css';
 import Layout from './src/components/layout/layout';
 import { ReactNode } from 'react';
+import ReactDOM from 'react-dom/client';
 
 type Args = {
   element: ReactNode;
@@ -11,4 +12,11 @@ type Args = {
 
 export const wrapPageElement = (args: Args) => {
   return <Layout {...args.props}>{args.element}</Layout>;
+};
+
+export const replaceHydrateFunction = () => {
+  return (element: any, container: any) => {
+    const root = ReactDOM.createRoot(container);
+    root.render(element);
+  };
 };
