4bec7a215240188ea9c903f0df27e7038a014e21
dom <mannadomenico2849@gmail.com>
2024-02-23 19:18:57 -0500

    Migrate class component to function component

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,42 +1,28 @@
-import { Component, ReactNode } from 'react';
+import { FunctionComponent, ReactNode, useState } from 'react';
 import Header from './header/header';
 import Footer from './footer/footer';
 import Container from '../ui/container/container';
 import * as styles from './layout.module.css';
 
-type State = {
-  hamburgerLinksShouldShow: boolean;
-};
-
 type Props = {
   children: ReactNode;
 };
 
-class Layout extends Component<Props, State> {
-  state: State = {
-    hamburgerLinksShouldShow: false,
-  };
+const Layout: FunctionComponent<Props> = ({ children }) => {
+  const [hamburgerLinksShouldShow, setHamburgerLinksShouldShow] = useState(false);
 
-  toggleHamburgerLinks = () => {
-    this.setState((prevState) => ({
-      hamburgerLinksShouldShow: !prevState.hamburgerLinksShouldShow,
-    }));
-  };
-
-  render() {
-    return (
-      <div className={styles.site}>
-        <Header
-          toggleHamburgerLinks={this.toggleHamburgerLinks}
-          hamburgerLinksShouldShow={this.state.hamburgerLinksShouldShow}
-        />
-        <main className={styles.mainContent}>
-          <Container> {this.props.children} </Container>
-        </main>
-        <Footer />
-      </div>
-    );
-  }
-}
+  return (
+    <div className={styles.site}>
+      <Header
+        toggleHamburgerLinks={() => setHamburgerLinksShouldShow((currentValue) => !currentValue)}
+        hamburgerLinksShouldShow={hamburgerLinksShouldShow}
+      />
+      <main className={styles.mainContent}>
+        <Container> {children} </Container>
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
 export default Layout;
