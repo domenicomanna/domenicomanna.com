bdee2920d0c1cf2256353ef8fd7fed0c671ba956
dom <mannadomenico2849@gmail.com>
2024-02-23 19:44:56 -0500

    Ignore mini-css-exract-plugin order

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -89,3 +89,14 @@ export const onCreateBabelConfig: GatsbyNode['onCreateBabelConfig'] = ({ actions
     },
   });
 };
+
+export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = ({ actions, getConfig }) => {
+  const config = getConfig();
+  const miniCssExtractPlugin = config.plugins.find(
+    (plugin: any) => plugin?.constructor?.name === 'MiniCssExtractPlugin'
+  );
+  if (miniCssExtractPlugin) {
+    miniCssExtractPlugin.options.ignoreOrder = true;
+  }
+  actions.replaceWebpackConfig(config);
+};
