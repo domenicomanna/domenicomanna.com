93f76d7a7edb9c0acc398a118aabab4c1efc5f29
dom <mannadomenico2849@gmail.com>
2024-02-25 21:16:12 -0500

    Update wording

--- src/pages/home/summary/summary.tsx
+++ src/pages/home/summary/summary.tsx
@@ -9,7 +9,7 @@ const Summary = () => {
     <section className={styles.hero}>
       <h1 className={styles.name}>Hi, I&apos;m Domenico</h1>
       <p className={styles.description}>
-        I work as a software developer, and studied computer science at the University of Bridgeport.
+        {"I'm a software developer, and studied computer science at the University of Bridgeport"}
       </p>
       <div className={styles.buttonWrapper}>
         <Link to={projectsSectionId} smooth={true} offset={-115}>
